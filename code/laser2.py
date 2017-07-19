3c761326c689f630a4ead0411727c0e16acd918f
Longfei Fan <physflf@gmail.com>
2017-07-19 02:00:49 -0500

    laser2.py partially done

--- code/laser2.py
+++ code/laser2.py
@@ -14,7 +14,7 @@ import numpy as np
 class LaserTwoMode(object):
     """A class for two-mode lasers"""
     
-    def __init__(state, l, n_max, rt=False):
+    def __init__(self, state, l, n_max, rt=False):
         """
         To initialize a two-mode state.
         
@@ -36,24 +36,26 @@ class LaserTwoMode(object):
         ValueError:
             when paras are not valid
         """
-        if n > 0 and state:
+        if n_max > 0:
             if state == 'TMSS':
-                self.state = self.__TMSS(l, n_max)
+                self.state, self.aver_n = self.__TMSS(l, n_max)
             elif state == 'PS':
-                self.state = self.__PS(l, n_max)
+                self.state, self.aver_n = self.__PS(l, n_max)
             elif state == 'PA':
-                self.state = self.__PA(l, n_max)
+                self.state, self.aver_n = self.__PA(l, n_max)
             elif state == 'PSA':
-                self.state = self.__PSA(l, n_max)
+                self.state, self.aver_n = self.__PSA(l, n_max)
             elif state == 'PAS':
-                self.state = self.__PAS(l, n_max)
+                self.state, self.aver_n = self.__PAS(l, n_max)
             elif state == 'PCS':
-                self.state = self.__PCS(l, n_max, rt)
+                self.state, self.aver_n = self.__PCS(l, n_max, rt)
             else:
-                raise ValueError('{} is not a valid name of two mode state!'.format())
+                raise ValueError('\'{}\' is not a valid name of two mode state!'.format(state))
+        else:
+            raise ValueError('n_max must be larger than zero!')
         
     
-    def __TMSS(l, n_max):
+    def __TMSS(self, l, n_max):
         """
         Two-mode squeezed state |TMSS \rangle = sum_0^n \lambda^n |n, n\rangle
         
@@ -72,10 +74,11 @@ class LaserTwoMode(object):
         """
         state = np.sum([l**n * tensor(basis(n_max, n), basis(n_max, n)) \
                         for n in range(n_max)])
-        return state.unit()
+        aver_n = l**2 / (1 - l**2)
+        return state.unit(), aver_n
     
     
-    def __PS(l, n_max):
+    def __PS(self, l, n_max):
         """
         Photon subtracted state |PS> = a b |TMSS>
 
@@ -94,10 +97,11 @@ class LaserTwoMode(object):
         """
         state = np.sum([(n+1) * l**n * tensor(basis(n_max, n), basis(n_max, n)) \
                         for n in range(n_max)])
-        return state.unit()
+        aver_n = 2*l**2 * (2 + l**2) / (1 - l**4)
+        return state.unit(), aver_n
     
     
-    def __PA(l, n_max):
+    def __PA(self, l, n_max):
         """
         Photon added state |PA> = a^\dagger b^\dagger |TMSS>
 
@@ -116,23 +120,26 @@ class LaserTwoMode(object):
         """
         state = np.sum([(n+1) * l**n * tensor(basis(n_max, n+1), basis(n_max, n+1)) \
                         for n in range(n_max - 1)])
-        return state.unit()
+        aver_n = (1 + 4*l**2 + l**4) / (1 - l**4)
+        return state.unit(), aver_n
     
-    def __PSA(N, l):
+    def __PSA(self, l, n_max):
         """Photon added then subtracted state"""
         state = np.sum([(n+1)**2 * l**n * tensor(basis(n_max, n), basis(n_max, n)) \
                         for n in range(n_max)])
-        return state.unit()
+        aver_n = 2*l**2 * (8 + 33*l**2 + 18*l**4 + l**6) / (1 + 10*l**2 - 10*l**6 + l**8)
+        return state.unit(), aver_n
 
 
-    def __PAS(N, l):
+    def __PAS(self, l, n_max):
         """Photon subtracted then added state"""
         state = np.sum([(n+1)**2 * l**n * tensor(basis(n_max, n + 1), basis(n_max, n + 1)) \
-                        for n in rrange(n_max - 1)])
-        return state.unit()
+                        for n in range(n_max - 1)])
+        aver_n = (1 + 26*l**2 + 66*l**4 + 26*l**6 + l**8) / (1 + 10*l**2 - 10*l**6 - l**8)
+        return state.unit(), aver_n
     
 
-    def __PCS(N, l, rt_list):
+    def __PCS(self, l, n_max, rt):
         """
         States obtained by a coherent superposition operation of photon subtraction 
         and addition on two-mode squeezed state
@@ -153,10 +160,23 @@ class LaserTwoMode(object):
                         * tensor(basis(n_max, n + 2), basis(n_max, n)) for n in nlist[:-2]])
         state4 = np.sum([l**n * (n+1) * tensor(basis(n_max, n + 1), basis(n_max, n + 1)) \
                         for n in nlist[:-1]])
-        state = ta * tb * state1 + ta * rb * state2 + \
-                ra * tb * state3 + ra * rb * state4
+        state = (ta * tb * state1 + ta * rb * state2 + \
+                ra * tb * state3 + ra * rb * state4).unit()
+        aver_n = expect(num(n_max), state.ptrace(0))
 
-        return state.unit()
+        return state, aver_n
+    
+    
+    def get_state(self):
+        return self.state
+    
+    
+    def get_aver_n(self):
+        return self.aver_n
+    
+    
+    def vn_entropy(self):
+        return entropy_vn(self.state.ptrace(0))
         
         
         
