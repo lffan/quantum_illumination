0eede685d3fbf16548da4dfcdb05301e92042840
Longfei Fan <physflf@gmail.com>
2017-07-24 09:48:55 -0500

    laser2 inheritance created

--- src/laser2.py
+++ src/laser2.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Created on 2017-07-18
-# Modified on 2017-07-18
+# Modified on 2017-07-24
 
-""" two-mode laser """
+""" two-mode laser, TMSS and non-Gaussian states """
 
 __author__ = 'Longfei Fan'
 
@@ -14,7 +14,7 @@ import numpy as np
 class LaserTwoMode(object):
     """A class for two-mode lasers"""
     
-    def __init__(state, l, n_max, rt=False):
+    def __init__(state_name):
         """
         To initialize a two-mode state.
         
@@ -23,127 +23,125 @@ class LaserTwoMode(object):
         state: string
             can only be one of 'TMSS', 'PS', 'PA', 'PAS', 'PSA', and 'PCS'
             used to specify what kind of two-mode state will be created
-        l: double float
-            state parameter, for TMSS l = tanh(s), where s is the squeezed para
-        n_max: positive int
-            photon truncation number as we are doing numerical calculation
-            i.e. photon numbers can be in [0, n_max - 1]
-        rt: int list, optional
-            superposition factors used when state == "PCS"
             
         Raise 
         -----
         ValueError:
             when paras are not valid
         """
-        if n > 0 and state:
-            if state == 'TMSS':
-                self.state = self.__TMSS(l, n_max)
-            elif state == 'PS':
-                self.state = self.__PS(l, n_max)
-            elif state == 'PA':
-                self.state = self.__PA(l, n_max)
-            elif state == 'PSA':
-                self.state = self.__PSA(l, n_max)
-            elif state == 'PAS':
-                self.state = self.__PAS(l, n_max)
-            elif state == 'PCS':
-                self.state = self.__PCS(l, n_max, rt)
-            else:
-                raise ValueError('{} is not a valid name of two mode state!'.format())
+        if n > 0 and state_name in ('TMSS', 'PS', 'PA', 'PSA', 'PAS', 'PCS'):
+            self.name = state_name
+            self.state = None
+        else:
+            raise ValueError('{} is not a valid name of two mode state!'.format())
         
     
-    def __TMSS(l, n_max):
-        """
-        Two-mode squeezed state |TMSS \rangle = sum_0^n \lambda^n |n, n\rangle
-        
-        Parameters
-        ----------
-        l: double float
-            state parameter, for TMSS l = tanh(s), where s is the squeezed para
-        n_max: positive int
-            photon truncation number as we are doing numerical calculation
-            i.e. photon numbers can be in [0, n_max - 1]
-        
-        Return
-        ------
-        qutip.Qobj()
-            a qutip object, a TMSS in bra form (column vector)
-        """
-        state = np.sum([l**n * tensor(basis(n_max, n), basis(n_max, n)) \
+class TMSS(LaserTwoMode):
+    """
+    Two-mode squeezed state |TMSS \rangle = sum_0^n \lambda^n |n, n\rangle
+
+    Parameters
+    ----------
+    l: double float
+        state parameter, for TMSS l = tanh(s), where s is the squeezed para
+    n_max: positive int
+        photon truncation number as we are doing numerical calculation
+        i.e. photon numbers can be in [0, n_max - 1]
+
+    Return
+    ------
+    qutip.Qobj()
+        a qutip object, a TMSS in bra form (column vector)
+    """
+    def __init__(name, l, n_max):
+        super.__init__(name)
+        self.state = np.sum([l**n * tensor(basis(n_max, n), basis(n_max, n)) 
+                             for n in range(n_max)])
+    
+    
+class PS(LaserTwoMode):
+    """
+    Photon subtracted state |PS> = a b |TMSS>
+
+    Parameters
+    ----------
+    l: double float
+        state parameter, for TMSS l = tanh(s), where s is the squeezed para
+    n_max: positive int
+        photon truncation number as we are doing numerical calculation
+        i.e. photon numbers can be in [0, n_max - 1]
+
+    Return
+    ------
+    qutip.Qobj()
+        a qutip object, a photon subtracted state in bra form
+    """
+    def __init__(name, l, n_max):
+        super().__init__(name)
+        self.state = np.sum([(n+1) * l**n * tensor(basis(n_max, n), basis(n_max, n)) 
                         for n in range(n_max)])
-        return state.unit()
     
-    
-    def __PS(l, n_max):
-        """
-        Photon subtracted state |PS> = a b |TMSS>
+class PA(LaserTwoMode):
+    """
+    Photon added state |PA> = a^\dagger b^\dagger |TMSS>
 
-        Parameters
-        ----------
-        l: double float
-            state parameter, for TMSS l = tanh(s), where s is the squeezed para
-        n_max: positive int
-            photon truncation number as we are doing numerical calculation
-            i.e. photon numbers can be in [0, n_max - 1]
-        
-        Return
-        ------
-        qutip.Qobj()
-            a qutip object, a photon subtracted state in bra form
-        """
-        state = np.sum([(n+1) * l**n * tensor(basis(n_max, n), basis(n_max, n)) \
-                        for n in range(n_max)])
-        return state.unit()
-    
-    
-    def __PA(l, n_max):
-        """
-        Photon added state |PA> = a^\dagger b^\dagger |TMSS>
+    Parameters
+    ----------
+    l: double float
+        state parameter, for TMSS l = tanh(s), where s is the squeezed para
+    n_max: positive int
+        photon truncation number as we are doing numerical calculation
+        i.e. photon numbers can be in [0, n_max - 1]
 
-        Parameters
-        ----------
-        l: double float
-            state parameter, for TMSS l = tanh(s), where s is the squeezed para
-        n_max: positive int
-            photon truncation number as we are doing numerical calculation
-            i.e. photon numbers can be in [0, n_max - 1]
-        
-        Return
-        ------
-        qutip.Qobj()
-            a qutip object, a photon added state in bra form
-        """
-        state = np.sum([(n+1) * l**n * tensor(basis(n_max, n+1), basis(n_max, n+1)) \
-                        for n in range(n_max - 1)])
-        return state.unit()
+    Return
+    ------
+    qutip.Qobj()
+        a qutip object, a photon added state in bra form
+    """
+    def __init__(name, l, n_max):
+        super().__init__(name)
+        self.state = np.sum([(n+1) * l**n * tensor(basis(n_max, n+1), basis(n_max, n+1)) 
+                             for n in range(n_max - 1)])
     
-    def __PSA(N, l):
-        """Photon added then subtracted state"""
-        state = np.sum([(n+1)**2 * l**n * tensor(basis(n_max, n), basis(n_max, n)) \
-                        for n in range(n_max)])
-        return state.unit()
+class PSA(LaserTwoMode):
+    """Photon added then subtracted state"""
+    def __init__(name, l, n_max):
+        super().__init__(name)
+        self.state = np.sum([(n+1)**2 * l**n * tensor(basis(n_max, n), basis(n_max, n)) 
+                             for n in range(n_max)])
 
-
-    def __PAS(N, l):
+class PAS(LaserTwoMode):
         """Photon subtracted then added state"""
         state = np.sum([(n+1)**2 * l**n * tensor(basis(n_max, n + 1), basis(n_max, n + 1)) \
                         for n in rrange(n_max - 1)])
         return state.unit()
     
 
-    def __PCS(N, l, rt_list):
-        """
-        States obtained by a coherent superposition operation of photon subtraction 
-        and addition on two-mode squeezed state
-        
-        Parameters
-        ----------
-        rt_list: a list of int
-            define the superposition factor
-        """
-        ta, ra, tb, rb = rt
+class PCS(LaserTwoMode):
+    """
+    States obtained by a coherent superposition operation of photon subtraction 
+    and addition on two-mode squeezed state
 
+    l: double float
+        state parameter, for TMSS l = tanh(s), where s is the squeezed para
+    n_max: positive int
+        photon truncation number as we are doing numerical calculation
+        i.e. photon numbers can be in [0, n_max - 1]
+    rt: int list, optional
+        superposition factors used when state == "PCS"
+
+    Parameters
+    ----------
+    rt_list: a list of int
+        define the superposition factor
+    """
+    def __init__(name, l, n_max, rt):
+        super().__init__(name)
+        self.state = self.__create_state(l, n_max, rt)
+
+            
+    def __create_state(l, n_max, rt):
+                ta, ra, tb, rb = rt
         nlist = range(n_max)
         state1 = np.sum([l**(n+1) * (n+1) * tensor(basis(n_max, n), basis(n_max, n)) \
                         for n in nlist])
@@ -153,35 +151,5 @@ class LaserTwoMode(object):
                         * tensor(basis(n_max, n + 2), basis(n_max, n)) for n in nlist[:-2]])
         state4 = np.sum([l**n * (n+1) * tensor(basis(n_max, n + 1), basis(n_max, n + 1)) \
                         for n in nlist[:-1]])
-        state = ta * tb * state1 + ta * rb * state2 + \
+        self.state = ta * tb * state1 + ta * rb * state2 + \
                 ra * tb * state3 + ra * rb * state4
-
-        return state.unit()
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-            
